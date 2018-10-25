0265163bfd7132fa1ccdfceb8657f92efa6f75da
Guordek <30309206+Guordek@users.noreply.github.com>
2018-10-25 19:17:29 +0200

    restructuration + add page "page" add/edit/remove

--- app/controllers/database/admin_page_db.js
+++ app/controllers/database/admin_page_db.js
@@ -1,3 +1,6 @@
+/**
+ * NEWS PART
+**/
 
 exports.addNews = function(news) {
     return  "INSERT INTO Page (`Title`, `Content`, `AdminId`, `Published_date`, `Updated_date`, `Lang`, `IdPageLang`, `IsNews`) " +
@@ -20,4 +23,31 @@ exports.getNewsById = function (id) {
 
 exports.deleteNews = function(id) {
   return "DELETE FROM Page WHERE `PageId` = " + id;
+};
+
+/**
+ * PAGE PART
+ **/
+
+exports.getPages = function() {
+    return "SELECT * FROM Page WHERE `IsNews` = 0";
+};
+
+exports.addPage = function(news) {
+    return  "INSERT INTO Page (`Title`, `Content`, `AdminId`, `Published_date`, `Updated_date`, `Lang`, `IdPageLang`, `IsNews`) " +
+        "VALUES ('" + news.Title + "', '" + news.Content + "', '" + news.AdminId + "', '" + news.Published_date + "', '" + news.Updated_date + "', '" + news.Lang + "', '" + news.IdPageLang + "', '" + news.IsNews + "')";
+};
+
+exports.deletePage = function(id) {
+    return "DELETE FROM Page WHERE `PageId` = " + id;
+};
+
+exports.getPageById = function (id) {
+    return "SELECT * FROM Page WHERE `IsNews` = 0 AND `PageId` = " + id;
+};
+
+exports.editPage = function(id, title, content, updated_date) {
+    return  "UPDATE Page " +
+        "SET `Title` = '" + title + "', `Content` = '" + content + "', `Updated_date` = '" + updated_date + "' " +
+        "WHERE `PageId` = " + id;
 };
