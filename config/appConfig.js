b2c604dd3ae8f4dba5a22d00844cdf38e5cbd5e1
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-11-08 00:39:31 +0100

    change root path appconfig

--- config/appConfig.js
+++ config/appConfig.js
@@ -21,17 +21,7 @@ const config = {
 
 */
     production: {
-        root: rootPath,
-        app: {
-            name: 'chanter-webapp'
-        },
-        port: 3000,
-        db: db.createConnection({
-            host: "mariadb",
-            database: "chanter-dev",
-            user: "dev",
-            password: "unlucky"
-        }),
+       
 
     },
 
