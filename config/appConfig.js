d4b8fb3baa540bd8d2edf6c82f4ad7807cbeb681
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-11-08 00:10:14 +0100

    remove  port for appconfig

--- config/appConfig.js
+++ config/appConfig.js
@@ -25,7 +25,6 @@ const config = {
         app: {
             name: 'chanter-webapp'
         },
-        port: 3000,
         db: db.createConnection({
             host: "mariadb",
             database: "chanter-dev",
